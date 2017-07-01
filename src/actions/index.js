8cae4fb2b164ecbfbe38bab058dc6019519ba8a1
Andy Tuttle <andytuttle12@gmail.com>
2017-07-01 04:26:41 -0400

    New project for Udemy redux course with Stephen Grider.

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'f3675291bdb0ee8b0f5c9b5e9ad3f66d';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
