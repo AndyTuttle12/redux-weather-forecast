715f9e8179e0a56de8cbd36299b05869a7ff06da
Andy Tuttle <andytuttle12@gmail.com>
2017-07-03 21:24:46 -0400

    Fixed reducer bug, and added new charts with styling and functional components.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,6 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {
     weather: WeatherReducer
-  }) => state
 });
-console.log(rootReducer.state)
 export default rootReducer;
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,10 @@
 import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
-    console.log(state)
     switch (action.type) {
     case 'FETCH_WEATHER':
         return [ action.payload.data, ...state ];
+    default:
+        return state;
     }
-    return state;
 }
